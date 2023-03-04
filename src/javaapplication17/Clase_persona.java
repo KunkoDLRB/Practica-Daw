2ea23d51ce2375c29c1de7b45fd594da1f766776
Kike <Kike@DESKTOP-ORLKRME>
2023-03-04 12:20:50 +0100

    Commit 0:Inicio

---
+++ src/javaapplication17/Clase_persona.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication17;
+
+/**
+ *
+ * @author Kike
+ */
+public class Clase_persona {
+    /**
+     * Creo una variable para la clase.
+     * @param Nombre es el nombre de la variable
+     */
+    private String Nombre;
+    
+    /**
+     * Creo una variable para la clase.
+     * @param Nom es el nombre de la variable que recibe
+     */
+    public void setNombre(String Nom){
+    this.Nombre=Nom;
+    }
+    /**
+     * Creo una variable para la clase.
+     * @return String es el tipo que se devuelve 
+     */
+    public String getNombre(){
+    return this.Nombre;
+    }
+}
